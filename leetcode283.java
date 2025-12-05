22ee2aa00170e1ce6092e29b44863a025c69d9c9
Lithika Murugesan <lithika2404@gmail.com>
2025-12-05 20:17:25 +0530

    leetcode283

---
+++ leetcode283.java
@@ -0,0 +1,23 @@
+public class leet {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {0,1,0,3,12};
+        s.moveZeroes(nums);
+
+        for (int x : nums) System.out.print(x + " ");
+    }
+}
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
