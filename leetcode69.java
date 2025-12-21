40e6251606eb7b6538f92df419133aba1a4c0b4d
Lithika Murugesan <lithika2404@gmail.com>
2025-12-21 13:05:48 +0530

    leetcode69.java

---
+++ leetcode69.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1)
+            return x;
+
+        int i;
+        for (i = 1; i <= x / i; i++) {
+           
+        }
+        return i - 1;
+    }
+}
+
