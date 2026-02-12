ccf599bd440e0548adc70f70b886a835a092f5d9
Lithika Murugesan <lithika2404@gmail.com>
2026-02-12 23:33:46 +0530

    ReverseArray.java

---
+++ ReverseArray.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class Tcs200 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] temp = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+
+            temp[i] = arr[n - i - 1];
+        }
+            for (int i = 0; i < n; i++) {
+                arr[i] = temp[i];
+                System.out.print(arr[i]);
+            }
+
+
+
+        }
+    }
