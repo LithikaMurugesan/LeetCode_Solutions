23e2b074fd9cf07aba442472ddb6d90e72fad225
Lithika Murugesan <lithika2404@gmail.com>
2026-02-21 22:40:18 +0530

    leetcode762.java

---
+++ leetcode762.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            int bits = Integer.bitCount(i);
+            if (isprime(bits)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static boolean isprime(int n) {
+        if (n < 2)
+            return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
