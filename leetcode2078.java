2d07a1bddf1af66a63fa59dcf370ab3da09c3df1
Lithika Murugesan <lithika2404@gmail.com>
2026-04-20 11:03:43 +0530

    leetcode2078.java

---
+++ leetcode2078.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDistance(int[] colors) {
+        int maxDist = 0;
+        for(int i =0;i<colors.length;i++){
+            if(colors[i]!=colors[0]){
+                maxDist = Math.max(maxDist,i);
+            }
+        }
+        for(int i=colors.length-1;i>=0;i--){
+            if(colors[i]!=colors[colors.length-1]){
+                maxDist = Math.max(maxDist,colors.length-1-i);
+            }
+        }
+        return maxDist;
+    }
+}
